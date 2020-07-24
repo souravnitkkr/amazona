ab675da0218996e552d89b4d120939b5b384178d
Kumar Sourav <kumars@maqsoftware.net>
2020-07-25 02:24:53 +0530

    first commit

---
+++ amazona/amazona/README.md
@@ -0,0 +1,33 @@
+# Amazona: An ECommerce Website like Amazon.com
+
+Demo : https://amazonaapp.herokuapp.com/
+
+## Table of Conent
+
+- Video-01-Intro
+- Video-02-Install-VSCode-Chrome
+- Video-03-Website-Template
+- Video-04-Product-List
+- Video-05-Sidebar
+- Video-06-Create-React-App
+- Video-07-Render-Products-Array
+- Video-08-React-Router-Product-Details
+- Video-09-Create-Node-Express-Server
+- Video-10-Fetch-Server-Data-Using-React-Hooks
+- Video-11-Manage-State-With-Redux
+- Video-12-Add-Redux-To-Product-Details
+- Video-13-Shopping-Cart-Screen
+- Video-14-Connect-to-MongoDB
+- Video-15-Signin-Register-Users
+- Video-16-Manage-Products-Screen
+  1. isAuth token = req.headers.authorization token.slice(7, token.length)
+  2. Create ProductsScreen.js and add to Add Route to App.js
+  3. product list 
+  4.  Define State for id, name, brand, category, image, price, countInStock, description
+- Video-17-Checkout-Wizard-Screen
+- Video-18-Order-Details-Screen
+- Video-19-Connect-to-Paypal
+- Video-20-User-Profile
+- Video-21-Manage-Order-Screen
+- Video-22-Filter-Sort-Products
+- Video-23-Deploy-Website-on-Heroku
---
+++ amazona/amazona/frontend/src/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+function App() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+        </button>
+            <Link to="/" >amazona</Link>
+          </div>
+          <div className="header-links">
+            <Link to="/cart/:id?">Cart</Link>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul className="categories">
+            <li>
+            <Link to="/category/Pants">Pants</Link>
+            </li>
+
+            <li>
+            <Link to="/category/Shirts">Shirts</Link>
+            </li>
+
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+          <Route path="/products" component={ProductsScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
+    </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
